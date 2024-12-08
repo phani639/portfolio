517573bc3126b19a9701608a46f0d1318763ed78
phani639 <phanigandi4@gmail.com>
2024-12-08 20:02:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('hiremeBtn').addEventListener('click', function() {
+    alert('Thank you for considering me! I look forward to working with you.');
+ });
+
+
+ document.getElementById('btntag').addEventListener('mouseover', function() {
+    this.style.backgroundColor = 'yellow';
+ });
+
+ document.getElementById('btntag').addEventListener('mouseout', function() {
+    this.style.backgroundColor = 'green';
+ });
